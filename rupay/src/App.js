58a40943a951ab4bd5a8bf809daa5d7110cfb9a9
Mateus Tavares <mateustavarestv@hotmail.com>
2023-06-24 16:26:26 -0300

    page confirm singup done

--- rupay/src/App.js
+++ rupay/src/App.js
@@ -1,11 +1,11 @@
-import Comments from './pages/comment/Comments';
-import MakeComment from './pages/comment/MakeComment';
-import PaginaInicial from './pages/home/PaginaInicial';
-import Login from './pages/login/login';
-import Menu from './pages/menu/Menu';
-import Select_menu from './pages/menu/Select_menu';
-import ErrorPay from './pages/feedback/ErrorPay';
-import Cadastro from './pages/singUp/SingUp';
+// import Comments from './pages/comment/Comments';
+// import MakeComment from './pages/comment/MakeComment';
+// import PaginaInicial from './pages/home/PaginaInicial';
+// import Login from './pages/login/login';
+// import Menu from './pages/menu/Menu';
+// import Select_menu from './pages/menu/Select_menu';
+// import ErrorPay from './pages/feedback/ErrorPay';
+// import Cadastro from './pages/singUp/SingUp';
 import Confirmao from './pages/singUp/SingUp_code';
 
 
